e4d3ae93c09a5f83fd4c9113ad40d76e73988d7a
faturachmanugraha <faturachman.work@gmail.com>
2021-03-17 14:00:46 +0700

    Adding MVC

---
+++ Overtime_API/Overtime_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Overtime_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
